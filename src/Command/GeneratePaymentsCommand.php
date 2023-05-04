05732d0abcb2a9beb8220db74fac751d6ae966ff
alaeddine <saidi.aladdin4@gmail.com>
2023-05-04 23:43:43 +0100

    feat: Add console command for generating paymentdates

---
+++ src/Command/GeneratePaymentsCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\SalesPayrollService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:generate-payments',
+    description: 'Generate payment dates for the sales department',
+)]
+class GeneratePaymentsCommand extends Command
+{
+    public function __construct(private readonly SalesPayrollService $payrollService)
+    {
+        parent::__construct(self::getDefaultName());
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHelp('This command allows you to generate a CSV file containing the payment dates for the sales department.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $now = new \DateTimeImmutable();
+        $year = $now->format('Y');
+
+        $paymentDates = [];
+
+        for ($month = 1; $month <= 12; $month++) {
+            $monthName = date('F', mktime(0, 0, 0, $month, 1));
+            $baseSalaryDate = $this->payrollService->getBaseSalaryDate($year, $month);
+            $bonusDate = $this->payrollService->getBonusDate($year, $month);
+
+            $paymentDates[] = [$monthName, $baseSalaryDate, $bonusDate];
+        }
+
+        $csvFile = fopen('payment-dates.csv', 'w');
+        fputcsv($csvFile, ['Month', 'Base Salary Payment Date', 'Bonus Payment Date']);
+
+        foreach ($paymentDates as $paymentDate) {
+            fputcsv($csvFile, $paymentDate);
+        }
+
+        fclose($csvFile);
+
+        $output->writeln('Payment dates have been generated in payment-dates.csv');
+
+        return Command::SUCCESS;
+    }
+}
+
---
+++ src/Service/SalesPayrollService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+class SalesPayrollService
+{
+    /**
+     * @param $year
+     * @param $month
+     * @return string
+     */
+    public function getBaseSalaryDate($year, $month): string
+    {
+        $lastDayOfMonth = new \DateTimeImmutable("last day of $year-$month");
+
+        // If last day of month is Sat or Sun
+        if ($lastDayOfMonth->format('N') >= 6) {
+            $lastDayOfMonth = $lastDayOfMonth->modify('last friday');
+        }
+
+        return $lastDayOfMonth->format('Y-m-d');
+    }
+
+    /**
+     * @param $year
+     * @param $month
+     * @return string
+     */
+    public function getBonusDate($year, $month): string
+    {
+        $bonusDate = new \DateTimeImmutable("$year-$month-15");
+
+        // If 15th is Sat or Sun
+        if ($bonusDate->format('N') >= 6) {
+            $bonusDate = $bonusDate->modify('next wednesday');
+        }
+
+        return $bonusDate->format('Y-m-d');
+    }
+}
